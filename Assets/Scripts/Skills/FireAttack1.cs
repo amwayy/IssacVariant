6cace22fb465ca0924cb8ab713b36cfa7d7dbaf9
lwxin0805 <137153563+lwxin0805@users.noreply.github.com>
2023-10-18 17:13:39 +0800

    增加了火元素技能1、尝试merge
    增加了火元素技能1、尝试merge

---
+++ Assets/Scripts/Skills/FireAttack1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAttack1 : Skill
+{
+    //¹¥»÷Á¦=70 ;·ÀÓùÁ¦=30 ; ÑªÁ¿=50
+    [SerializeField] private int thisActionPointExpense = 1;
+    [SerializeField] private int damageMin = 65;
+    [SerializeField] private int damageMax = 75;
+    [SerializeField] private float thisCastTime = 1f;
+
+    private void Awake()
+    {
+        skillName = "Fire Attack1";
+        actionPointExpense = thisActionPointExpense;
+        castTime = thisCastTime;
+    }
+
+    public override void CastSkill(ISkillCaster skillCaster)
+    {
+        base.CastSkill(skillCaster);
+
+        skillCaster.SetAttack(damageMin, damageMax);
+
+    }
+}
